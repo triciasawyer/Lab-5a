f4d00d3ee0dc271d12d88c1e1c6636091d24ad29
Tricia Sawyer <tricia.sawyer03@gmail>
2023-03-17 15:19:23 -0500

    add question 4

--- js/app.js
+++ js/app.js
@@ -11,7 +11,7 @@ Test this function by hand in the console to get it working, and when you think 
 function sum(a, b) { //eslint-disable-line
   let sumNumber = a + b;
   let message = `The sum of ${a} and ${b} is ${sumNumber}.`;
-
+  //         0           1
   return[sumNumber, message];
 }
 
@@ -72,24 +72,37 @@ testSumAndMultiply(4,7,5);
 
 /////////////////////////////////////
 /* Problem 4
-Write a function called sumArray() that takes in an array of numbers as its single argument and then returns an array where the first element is the sum of the numbers in the array, and the second element is a string that EXACTLY follows this example and uses the values that were input into the function:
+Write a function called sumArray() X
+that takes in an array of numbers as its single argument and X
+then returns an array where X
+the first element is the sum of the numbers in the array, x
 
+and the second element is a string that EXACTLY follows this example and uses the values that were input into the function:
 "2,3,4 was passed in as an array of numbers, and 9 is their sum."
 
-IMPORTANT DETAIL: You may not use the arithmetic operator + in this function. To do addition, use your sum() function that you've already created. You're going to have to be resourceful to figure out how to do this. However, you may continue to use the + operator for string concatenation.
+IMPORTANT DETAIL: You may not use the arithmetic operator + in this function.
+To do addition, use your sum() function that you've already created.
+You're going to have to be resourceful to figure out how to do this. However, you may continue to use the + operator for string concatenation.
 
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSumArray() function and see if the test passes.*/
 
 // Write your code here
 let testArray = [2, 3, 4]; //eslint-disable-line
 
-function sumArray(sumArr) { //eslint-disable-line
 
+function sumArray(sumArr) { //eslint-disable-line
+  //                              sum(sum(a,b)[0], c)[0]
+//   console.log(sumArr[0]);
+  let sumArrayNum =    sum(sum(sumArr[0],sumArr[1])[0], sumArr[2])[0];
+  console.log('🚀 ~ file: app.js:97 ~ sumArray ~ sumArrayNum:', sumArrayNum);
+  let message = '2,3,4 was passed in as an array of numbers, and 9 is their sum.';
+
+  return [sumArrayNum, message];
 }
 
 // Here is the test for sumArray(); uncomment it to run it
 
-// testSumArray(testArray);
+testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
